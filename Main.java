daadb145e79c8521aab834f0a1f87ed11a07ba3e
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-19 11:30:20 +0530

    done

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out .println("The World is too cruel i see still now yet, need to discover up soon");
+        String u = "I am a programmer great WORLD done!";
+        System.out.print(u);
+        int a,b,c;
+        
     }
     
 }
