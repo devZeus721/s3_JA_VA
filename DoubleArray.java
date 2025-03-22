7ce48b3d69cab5f3ab53d13447b4cf7355d61467
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 17:41:54 +0530

    setup new file done

--- DoubleArray.java
+++
@@ -1,9 +0,0 @@
-public class DoubleArray {
-    public static void main(String[] args) {
-        double[][] A1 = { { 1, 2, 3, 4 }, { 5, 6, 7 }, { 8, 9 }, { 10 } };
-        int m = A1.length;
-        for (int j = 0; j < m; j++) {
-            System.out.print("NewArray " + " " + java.util.Arrays.toString(A1[j]));
-        }
-    }
-}
---
+++ dArr01.java
@@ -0,0 +1,11 @@
+public class dArr01 {
+        public static void main(String[] args) {
+            int[][] A1 = { { 1, 2, 3, 4 }, { 5, 6, 7 }, { 8, 9 }, { 10 } };
+            int m = A1.length;
+            for (int j = 0; j < m; j++) {
+                System.out.print("NewArray " + " " + (A1[j]));
+            }
+        }
+    }
+    
+}
