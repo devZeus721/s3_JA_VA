be12e04508969b500ce7efca2641daeaf8a90d22
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 11:52:12 +0530

    done new file comitted

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    String nam;
+    String idno;
+    int age;
+    char div;
+
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        st1.nam = "James";
+        st2.nam = "Roger";
+        st1.idno = "3433";
+        st2.idno = "3434";
+        st1.age = 21;
+        st1.age = 22;
+        st1.div = 'D';
+        st1.div = 'E';
+    }
+}
