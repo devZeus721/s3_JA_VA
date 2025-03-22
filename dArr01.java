7b1c92d3724bb922dab30837d4d86896f3de52ec
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 21:45:13 +0530

    redone everything

--- dArr01.java
+++ dArr01.java
@@ -2,8 +2,9 @@ public class dArr01{
     public static void main(String[] args) {
         int[][] A1 = { { 1, 2, 3, 4 }, { 5, 6, 7 }, { 8, 9 }, { 10 } };
         int m = A1.length;
+        System.out.println("NewArray " + " ");
         for (int j = 0; j < m; j++) {
-            System.out.print("NewArray " + " " + java.util.Arrays.toString(A1[j]));
+            System.out.print(java.util.Arrays.toString(A1[j]));
         }
     }
 }
