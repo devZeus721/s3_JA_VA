bccdb04da3861430095bd8ec0f11eb351acdf2e6
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 11:29:07 +0530

    done new

---
+++ oasm2.java
@@ -0,0 +1,14 @@
+public class oasm2 {
+    int x = 24;
+
+    public static void main(String[] args) {
+
+        oasm2 ob1 = new oasm2();
+        oasm2 ob2 = new oasm2();
+        oasm2 ob3 = new oasm2();
+        System.out.println(ob1.x + 2);
+        System.out.println(ob2.x + 4);
+        System.out.println(ob3.x + 6);
+    }
+
+}
