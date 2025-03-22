f1fa1c100e1c8d60249c1b0329ea454e1e1b14e2
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 21:25:52 +0530

    redone

--- dArr01.java
+++ dArr01.java
@@ -1,4 +1,4 @@
-public class dArr01 {
+public class dArr01{
     public static void main(String[] args) {
         int[][] A1 = { { 1, 2, 3, 4 }, { 5, 6, 7 }, { 8, 9 }, { 10 } };
         int m = A1.length;
