2808c02046f370951a9674a2b64d07bb2053ccea
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 17:32:52 +0530

    restiignfile loaction

--- DoubleArray.java
+++ DoubleArray.java
@@ -1,9 +1,9 @@
-public class double_array {
+public class DoubleArray {
     public static void main(String[] args) {
         double[][] A1 = { { 1, 2, 3, 4 }, { 5, 6, 7 }, { 8, 9 }, { 10 } };
         int m = A1.length;
-        for (int j = 0; i < m; j++) {
-            System.out.print("NewArray " + " " + (A1[j]));
+        for (int j = 0; j < m; j++) {
+            System.out.print("NewArray " + " " + java.util.Arrays.toString(A1[j]));
         }
     }
 }
