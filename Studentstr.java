83b02325a7aacf2c6df08b658e16b2f7432e105d
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 12:30:32 +0530

    Update Studentstr.java
    done settling

--- Studentstr.java
+++ Studentstr.java
@@ -1,4 +1,4 @@
-//string Usage comes here
+//String & Object Usage comes here
 public class Studentstr {
     String nam;//iniialing String
     String idno;//iniialing String
@@ -6,8 +6,10 @@ public class Studentstr {
     char div;
 
     public static void main(String[] args) {
-        Studentstr st1 = new Studentstr();
-        Studentstr st2 = new Studentstr();
+        
+        Studentstr st1 = new Studentstr();//object creation
+        Studentstr st2 = new Studentstr();//object creation
+        
         st1.nam = "James Steve";//string representing
         st2.nam = "Roger Federer";//string representing
         st1.idno = "3433";
@@ -16,10 +18,13 @@ public class Studentstr {
         st1.age = 22;
         st1.div = 'D';
         st1.div = 'E';
-        System.out.println("Idno of Student 1:" + st1.idno + " ");
+      
+        System.out.println("DETAILS of Students");
+        System.out.println(" ");
+        System.out.println("Idno of Student 1:" + st1.idno + " ");//string printing woith whitespace
         System.out.println("Name: " + st1.nam);
         System.out.println("Age & Div: " + st1.age + "  " + st1.div);
-        System.out.println("Idno of Student 2: " + st2.idno + " ");
+        System.out.println("Idno of Student 2: " + st2.idno + " ");//string printing woith whitespace
         System.out.println("Name: " + st2.nam);
         System.out.println("Age & Div: " + st1.age + "  " + st1.div);
     }
