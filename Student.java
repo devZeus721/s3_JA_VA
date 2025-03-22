dc37e2b507f4569aaaea7b1ee388b1a978b2d3eb
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 12:11:55 +0530

    done recommited

--- Student.java
+++ Student.java
@@ -7,13 +7,19 @@ public class Student {
     public static void main(String[] args) {
         Student st1 = new Student();
         Student st2 = new Student();
-        st1.nam = "James";
-        st2.nam = "Roger";
+        st1.nam = "James Steve";
+        st2.nam = "Roger Federer";
         st1.idno = "3433";
         st2.idno = "3434";
         st1.age = 21;
         st1.age = 22;
         st1.div = 'D';
         st1.div = 'E';
+        System.out.println("Idno of Student 1:" + st1.idno + " ");
+        System.out.println("Name: " + st1.nam);
+        System.out.println("Age & Div: " + st1.age + "  " + st1.div);
+        System.out.println("Idno of Student 2: " + st2.idno + " ");
+        System.out.println("Name: " + st2.nam);
+        System.out.println("Age & Div: " + st1.age + "  " + st1.div);
     }
 }
