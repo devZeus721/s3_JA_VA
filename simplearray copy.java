905721ae5510227e7041307e7e5c0d588395d008
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 17:21:54 +0530

    comiited done leaving out

--- simplearray copy.java
+++ simplearray copy.java
@@ -1,6 +1,6 @@
 public class simplearray {
     public static void main(String[] args) {
-
+        double[][] A1 = { { 1, 2, 3, 4 }, { 5, 6, 7 }, { 8, 9 }, { 10 } };
         char[] a1[] = { { 'h', 'e', 'l', 'l', 'o', ' ', 'J', 'A', 'V', 'A' } };/*
                                                                                 * String str = "abc" The above
                                                                                 * declaration is equivalent to: char
@@ -12,7 +12,10 @@ public class simplearray {
         for (int i = 0; i < n; i++) {
             System.out.print("Array is genereted:" + " " + new String(a1[i]));
             /* fix Must explicitly convert the char[] to a String */
-
+            int m = A1.length;
+            for (int j = 0; i < m; j++) {
+                System.out.print("NewArray " + " " + (A1[j]));
+            }
         }
     }
 }
