bd993fec05c99b0e68b33353b20fda83ae76656f
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-22 17:28:23 +0530

    renamed sucessfully

---
+++ double_array.java
@@ -0,0 +1,9 @@
+public class double_array {
+    public static void main(String[] args) {
+        double[][] A1 = { { 1, 2, 3, 4 }, { 5, 6, 7 }, { 8, 9 }, { 10 } };
+        int m = A1.length;
+        for (int j = 0; i < m; j++) {
+            System.out.print("NewArray " + " " + (A1[j]));
+        }
+    }
+}
